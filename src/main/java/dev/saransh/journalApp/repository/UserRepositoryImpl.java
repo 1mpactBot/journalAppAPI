ad252e0ab48f9b7e499c015d00f2a20b3f528556
Saransh Gautam <saransh79@gmail.com>
2024-10-20 11:24:47 +0530

    Added sendEmail for sentiment Analysis.

--- src/main/java/dev/saransh/journalApp/repository/UserRepositoryImpl.java
+++ src/main/java/dev/saransh/journalApp/repository/UserRepositoryImpl.java
@@ -1,4 +1,25 @@
 package dev.saransh.journalApp.repository;
 
+import dev.saransh.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
 public class UserRepositoryImpl {
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    //Getting list of users who have emails and had opted for sentiment analysis
+    public List<User> getUsersForSA() {
+        Query query = new Query();
+        // We can add regex for email
+        query.addCriteria(Criteria.where("email").exists(true));
+        query.addCriteria(Criteria.where("email").ne(null).ne(""));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        List<User> users = mongoTemplate.find(query, User.class);
+        return users;
+    }
 }
